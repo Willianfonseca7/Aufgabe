21460421548ac3764a5e95bebf265a22229d5dc4
willianfonseca <wfonseca7@hotmail.com>
2025-12-08 10:54:44 +0100

    Capture remaining setup changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
+// tailwind.config.js
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  darkMode: "class", // 👈 importante!
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
